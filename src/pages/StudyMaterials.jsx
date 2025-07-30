ca4a771bdc7ef4beb24d3bfb1da731ce1de163ef
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-07-31 01:35:59 +0530

    added a page where user can select syllabus block wise

--- src/pages/StudyMaterials.jsx
+++ src/pages/StudyMaterials.jsx
@@ -16,7 +16,6 @@ const StudyMaterials = () => {
     useAppContext();
 
   const { courseCode } = useParams();
-  console.log(courseCode);
 
   useEffect(() => {
     if (!courseCode) {
@@ -27,9 +26,17 @@ const StudyMaterials = () => {
     }
   }, [courseCode]);
 
-  // const selectedCourseTitle = semesters
-  //   .find((sem) => sem.title === selectedSemester)
-  //   .subjects.find((sub) => sub.code === selectedCourse)?.title;
+  const selectedCourseTitle = semesters
+    .find((sem) => sem.title === selectedSemester)
+    .subjects.find((sub) => sub.code === selectedCourse)?.title || "Select a Course";
+
+    console.log(selectedCourseTitle);
+
+  const selectedCourseMaterial = semesters
+    .find((sem) => sem.title === selectedSemester)
+    .subjects.find((sub) => sub.code === selectedCourse)?.material;
+
+    console.log(selectedCourseMaterial);
 
   // const selectedSessionCourseAssignmentLink = semesters
   //   .find((sem) => sem.title === selectedSemester)
@@ -42,55 +49,41 @@ const StudyMaterials = () => {
     <div className="flex flex-col">
       <HeroCarousel
         sectionHeading={"Study Materials"}
-        sectionDesc={"assignments"}
+        sectionDesc={"syllabus"}
         totalPages={2}
         baseUrl={"/study-materials"}
       >
         {/* Page - 2 */}
-        <div className="flex justify-between gap-4">
-          <div className="w-full flex flex-col justify-between">
-            <Link
-              to={`/study-materials`}
-              className="p-2 w-fit cursor-pointer border rounded-full border-b-3 border-r-2 bg-violet-50 border-violet-500 hover:-translate-y-1 transition-all duration-300 ease"
-            >
-              <img src={backIcon} width={20} className="brightness-20" />
-            </Link>
+        <>
+          <Link
+            to={"/study-materials"}
+            className="absolute p-2 bottom-4 left-1/2 -translate-x-1/2 cursor-pointer border rounded-full border-b-3 border-r-2 bg-violet-50 border-violet-500 hover:-translate-y-1 transition-all duration-300 ease"
+          >
+            <img src={backIcon} width={20} className="brightness-20" />
+          </Link>
+          <div className="flex items-center justify-between p-3 border border-gray-200 rounded border-b-3 cursor-pointer mb-4 text-gray-700">
             <div>
-              <h1 className="text-lg flex items-center gap-2">
-                {selectedCourse}
-                <p className="bg-green-100 text-green-600 text-sm w-fit px-2 rounded">
-                  Solved
-                </p>
-              </h1>
-              <h2>{}</h2>
-              <h3>Assignments </h3>
-
-              <div className="flex w-full gap-1 mt-4">
-                <a
-                  className="w-full text-center rounded py-2 cursor-pointer bg-blue-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-blue-600"
-                  target="_blank"
-                  href="/"
-                >
-                  {" "}
-                  Download Assignment
-                </a>
-
-                <br />
-                <button
-                  className={`w-full rounded py-2 cursor-pointer bg-green-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-green-600`}
-                >
-                  Download Solution
-                </button>
-              </div>
+              <h2 className="text-lg">
+                {courseCode&& courseCode.toUpperCase()} - {selectedCourseTitle || "Select a Course"}
+              </h2>
+              <p className=" text-text-primary/60">
+                Syllabus is organized block wise
+              </p>
             </div>
           </div>
-          <div className="relative h-82 border rounded-md border-gray-300 bg-blue-200 w-full overflow-hidden group">
-            <img
-              src={bcs012June2024}
-              className="absolute object-cover -rotate-25 shadow-2xl right-0 -bottom-4 translate-y-1/2 translate-x-1/3 group-hover:scale-105 transition-transform duration-300 ease-in-out"
-            />
-          </div>
-        </div>
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+            {selectedCourseMaterial &&
+              selectedCourseMaterial.map((material, index) => (
+                <Link
+                  to={material.link}
+                  key={index}
+                  className="border p-2 border-gray-200 hover:-translate-y-1 transition-all duration-300 ease rounded border-b-3 text-blue-600 cursor-pointer"
+                >
+                  {material.id} - {material.title}
+                </Link>
+              ))}
+          </ul>
+        </>
       </HeroCarousel>
       <FeaturedCarousel />
     </div>
