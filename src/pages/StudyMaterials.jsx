fc5c51ca0ca1f87b48d95dcf86ccc01339bfa1e1
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-08-02 15:50:48 +0530

    added page 3 in study-materials section

--- src/pages/StudyMaterials.jsx
+++ src/pages/StudyMaterials.jsx
@@ -50,7 +50,7 @@ const StudyMaterials = () => {
       <HeroCarousel
         sectionHeading={"Study Materials"}
         sectionDesc={"syllabus"}
-        totalPages={2}
+        totalPages={3}
         baseUrl={"/study-materials"}
       >
         {/* Page - 2 */}
@@ -84,6 +84,52 @@ const StudyMaterials = () => {
               ))}
           </ul>
         </>
+
+        {/* Page - 3 */}
+                <div className="flex justify-between gap-4">
+                  <div className="w-full flex flex-col justify-between">
+                    <Link
+                      to={`/study-materials/${courseCode}`}
+                      className="p-2 w-fit cursor-pointer border rounded-full border-b-3 border-r-2 bg-violet-50 border-violet-500 hover:-translate-y-1 transition-all duration-300 ease"
+                    >
+                      <img src={backIcon} width={20} className="brightness-20" />
+                    </Link>
+                    <div>
+                      <h1 className="text-lg flex items-center gap-2">
+                        {}
+                        <p className="bg-green-100 text-green-600 text-sm w-fit px-2 rounded">
+                          Solved
+                        </p>
+                      </h1>
+                      <h2>{selectedCourseTitle}</h2>
+                      <h3>Previous Year Question Paper - {}</h3>
+        
+                      <div className="flex w-full gap-1 mt-4">
+                        <a
+                          className="w-full text-center rounded py-2 cursor-pointer bg-blue-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-blue-600"
+                          target="_blank"
+                          href=""
+                        >
+                          {" "}
+                          Download Paper
+                        </a>
+        
+                        <br />
+                        <button
+                          className={`w-full rounded py-2 cursor-pointer bg-green-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-green-600`}
+                        >
+                          Download Solution
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative h-82 border rounded-md border-gray-300 bg-blue-200 w-full overflow-hidden group">
+                    <img
+                      src={bcs012June2024}
+                      className="absolute object-cover -rotate-25 shadow-2xl right-0 -bottom-4 translate-y-1/2 translate-x-1/3 group-hover:scale-105 transition-transform duration-300 ease-in-out"
+                    />
+                  </div>
+                </div>
       </HeroCarousel>
       <FeaturedCarousel />
     </div>
