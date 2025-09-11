2945b0ca5f9070fca6880cefc75fd454ed4697d3
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-11 11:35:04 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import FeaturedCarousel from "../components/FeaturedCarousel";
 const Home = () => {
   return (
     <>
-      <div className="h-[calc(calc(100dvh-164px)/2)] rounded-md overflow-hidden">
+      <div className="h-[calc(calc(100vh-164px)/2)] rounded-md overflow-hidden">
         <img
           src={heroImg}
           className="h-full w-full object-cover object-bottom"
           alt=""
         />
       </div>
-      <div className="h-[calc(calc(100dvh-164px)/2)] overflow-hidden">
+      <div className="h-[calc(calc(100vh-164px)/2)] overflow-hidden">
         <FeaturedCarousel />
       </div>
     </>
