df0f772e64302d11beef640696c722c8ea396b2d
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-11 18:33:46 +0530

    some changes

--- src/pages/PYQ.jsx
+++ src/pages/PYQ.jsx
@@ -79,7 +79,7 @@ const PYQ = () => {
             >
               <img src={backIcon} className="w-4 h-auto brightness-20" alt="" />
             </Link>
-            <div className="mt-2">
+            <div>
               <h1 className="text-lg">{selectedCourse}</h1>
               <h2>{selectedCourseTitle}</h2>
               <p className=" text-text-primary/60">
@@ -119,44 +119,42 @@ const PYQ = () => {
           </>
 
           {/* Page - 3 */}
-          <div className="flex flex-col lg:flex-row justify-between gap-4 hidden">
-            <div className="w-full flex flex-col justify-between">
-              <Link
-                to={`/previous-year-question-papers/${courseCode}`}
-                className="p-2 w-fit cursor-pointer border rounded-full border-b-3 border-r-2 bg-violet-50 border-violet-500 hover:-translate-y-1 transition-all duration-300 ease"
-              >
-                <img src={backIcon} width={20} className="brightness-20" />
-              </Link>
-              <div>
-                <h1 className="text-lg flex items-center gap-2">
-                  {selectedCourse}
-                  <p className="bg-green-100 text-green-600 text-sm w-fit px-2 rounded">
-                    Solved
-                  </p>
-                </h1>
-                <h2>{selectedCourseTitle}</h2>
-                <h3>Previous Year Question Paper - {selectedYear}</h3>
+          <div className="flex flex-col lg:flex-row justify-between gap-4">
+            <Link
+              to={`/previous-year-question-papers/${courseCode}`}
+              className="absolute right-0 flex items-center justify-center w-8 h-8 bg-violet-200 border border-r-2 border-b-2 border-violet-300 rounded-full"
+            >
+              <img src={backIcon} className="w-4 h-auto brightness-20" alt="" />
+            </Link>
+            <div className="flex flex-col justify-between">
+              <h1 className="text-lg flex items-center gap-2">
+                {selectedCourse}
+              </h1>
+              <p className="bg-green-100 text-green-600 text-sm w-fit px-2 rounded">
+                Solved
+              </p>
+              <h2>{selectedCourseTitle}</h2>
+              <h3>Previous Year Question Paper - {selectedYear}</h3>
 
-                <div className="flex w-full gap-1 mt-4">
-                  <a
-                    className="w-full text-center rounded py-2 cursor-pointer bg-blue-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-blue-600"
-                    target="_blank"
-                    href={selectedCoursePaperLink}
-                  >
-                    {" "}
-                    Download Paper
-                  </a>
+              <div className="flex lg:flex-row w-full gap-1 mt-4 ">
+                <a
+                  className="w-full block text-center rounded py-2 cursor-pointer bg-blue-200 hover:-translate-y-1 transition duration-300 ease-in-out text-blue-600"
+                  target="_blank"
+                  href={selectedCoursePaperLink}
+                >
+                  {" "}
+                  Download Paper
+                </a>
 
-                  <br />
-                  <button
-                    className={`w-full rounded py-2 cursor-pointer bg-green-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-green-600`}
-                  >
-                    Download Solution
-                  </button>
-                </div>
+                <br />
+                <button
+                  className={`w-full block rounded py-2 cursor-pointer bg-green-200 hover:-translate-y-1 transition duration-300 ease-in-out text-green-600`}
+                >
+                  Download Solution
+                </button>
               </div>
             </div>
-            <div className="relative h-82 border rounded-md border-gray-300 bg-blue-200 w-full overflow-hidden group">
+            <div className="relative border rounded-md border-gray-300 bg-blue-200 w-full overflow-hidden group hidden">
               <img
                 src={bcs012June2024}
                 className="absolute object-cover -rotate-25 shadow-2xl right-0 -bottom-4 translate-y-1/2 translate-x-1/3 group-hover:scale-105 transition-transform duration-300 ease-in-out"
