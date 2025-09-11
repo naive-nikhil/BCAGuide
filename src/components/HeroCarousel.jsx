ea264e87391d08843094d33add225c372a542ade
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-11 15:41:40 +0530

    some changes

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -89,7 +89,7 @@ const HeroCarousel = ({ sectionDesc, totalPages, children, baseUrl }) => {
                     setPage(1);
                     setSelectedSemester(semester.title);
                   }}
-                  className={`p-3 lg:p-0 lg:h-100 flex items-center justify-center rounded-md w-full text-nowrap bg-violet-200 border border-violet-300 border-b-2 hover:border-violet-400 ${
+                  className={`p-3 lg:p-0 lg:h-14 flex items-center justify-center rounded-md w-full text-nowrap bg-violet-200 border border-violet-300 border-b-2 hover:border-violet-400 ${
                     selectedSemester === semester.title
                       ? "border-violet-400"
                       : ""
@@ -105,7 +105,7 @@ const HeroCarousel = ({ sectionDesc, totalPages, children, baseUrl }) => {
             </ul>
           </div>
 
-          <div className="flex-1 bg-white p-2 lg:p-4">
+          <div className="flex-1 bg-white p-2 lg:p-4 overflow-auto">
             <div
               style={{
                 width: `${totalPages * 100}%`,
@@ -114,15 +114,15 @@ const HeroCarousel = ({ sectionDesc, totalPages, children, baseUrl }) => {
               className={`flex transition-transform h-full duration-500 ease-in-out`}
             >
               {/* Page 1 - Will remain same across the app */}
-              <div className="w-full [&_h2]:cursor-pointer h-full">
-                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                  <li className="p-3 border border-gray-200 rounded border-b-3 cursor-pointer">
-                    <h2 className="text-lg">Courses in {selectedSemester}</h2>
-                    <p className=" text-text-primary/60">
-                      This section contains {sectionDesc} of{" "}
-                      {selectedSemester.toLowerCase()} courses!
-                    </p>
-                  </li>
+              <div className="w-full [&_h2]:cursor-pointer flex flex-col gap-4">
+                <section>
+                  <h1 className="text-lg">Courses in {selectedSemester}</h1>
+                  <p className="text-gray-400">
+                    This section contains {sectionDesc} of{" "}
+                    {selectedSemester.toLowerCase()} courses!
+                  </p>
+                </section>
+                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 pb-4">
                   {semesters
                     .find((sem) => sem.title === selectedSemester)
                     .subjects.map((sub, index) => (
@@ -132,7 +132,9 @@ const HeroCarousel = ({ sectionDesc, totalPages, children, baseUrl }) => {
                         className="p-3 border border-gray-200 rounded border-b-3 cursor-pointer hover:-translate-y-1 transition-all duration-300 ease"
                       >
                         <h2 className="text-blue-600">{sub.code}</h2>
-                        <p className=" text-text-primary/60">{sub.title}</p>
+                        <p className=" text-text-primary/60 line-clamp-1">
+                          {sub.title}
+                        </p>
                       </Link>
                     ))}
                 </ul>
