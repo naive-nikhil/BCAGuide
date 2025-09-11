d99f0e8ffd02f2daba1215c952607148a979c1dc
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-11 13:52:08 +0530

    some changes

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -64,7 +64,6 @@ const Project = () => {
             <div className="flex-1 bg-white p-2 lg:p-4 h-full overflow-auto">
               {activeStep?.component}
             </div>
-            0
           </div>
 
           <div className="relative hidden 2xl:block overflow-hidden w-80 h-150">
