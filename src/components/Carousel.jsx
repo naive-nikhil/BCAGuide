85e26d2506a58404b5fcb59f36e70695fe2aa5df
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-11 21:19:39 +0530

    some changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,22 @@
-const Carousel = () => {
+import heroImg from "../assets/hero.jpg";
+
+const Carousel = ({ sidebarComponent, bodyComponent }) => {
   return (
-    <div className="flex flex-col lg:flex-row w-full">
-      <div className="bg-violet-100 p-2 lg:h-full flex flex-col gap-6 items-center text-lg text-gray-700">
-        <h1 className="p-3 bg-violet-300 rounded-md w-full text-center">
-          Steps to complete this course
-        </h1>
-        <div className="flex flex-row lg:flex-col gap-2 lg:gap-6 text-nowrap w-full select-none">
-          {steps.map((step) => (
-            <Step
-              key={step.key}
-              stepKey={step.key}
-              label={step.label}
-              shortLabel={step.shortLabel}
-              number={step.number}
-              selectedStep={selectedStep}
-              setSelectedStep={setSelectedStep}
-            />
-          ))}
+    <div className="flex w-full justify-between overflow-hidden rounded-md">
+      <div className="flex flex-col lg:flex-row w-full">
+        {sidebarComponent}
+
+        <div className="flex-1 bg-white p-2 lg:p-4 h-full overflow-auto">
+          {bodyComponent}
         </div>
       </div>
-      <div className="flex-1 bg-white p-2 lg:p-4 h-full overflow-auto">
-        {activeStep?.component}
+
+      <div className="relative hidden 2xl:block overflow-hidden w-80 h-150">
+        <img
+          src={heroImg}
+          alt="Image representing a wooden block on table with text 'Do What You Love' written on it."
+          className="absolute w-full h-full object-cover -top-50"
+        />
       </div>
     </div>
   );
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import FeaturedCarousel from "../components/FeaturedCarousel";
-import heroImg from "../assets/hero.jpg";
-
-
 import StepList from "../components/project/StepList";
-import SemesterList from "../components/carousel/SemesterList";
 
 import { useAppContext } from "../context/AppContext";
 import { getActiveStepComponent } from "../components/project/StepList";
+import Carousel from "../components/Carousel";
 
 const Project = () => {
   const { selectedStep } = useAppContext();
@@ -18,23 +14,10 @@ const Project = () => {
         <h1 className="text-xl text-gray-700">
           Project Synopsis & Report (BCSP064)
         </h1>
-        <div className="flex w-full justify-between overflow-hidden rounded-md">
-          <div className="flex flex-col lg:flex-row w-full">
-            <StepList />
-
-            <div className="flex-1 bg-white p-2 lg:p-4 h-full overflow-auto">
-              {ActiveStepComponent}
-            </div>
-          </div>
-
-          <div className="relative hidden 2xl:block overflow-hidden w-80 h-150">
-            <img
-              src={heroImg}
-              alt="Image representing a wooden block on table with text 'Do What You Love' written on it."
-              className="absolute w-full h-full object-cover -top-50"
-            />
-          </div>
-        </div>
+        <Carousel
+          sidebarComponent={<StepList />}
+          bodyComponent={ActiveStepComponent}
+        />
       </section>
       <section className="h-[calc(calc(100vh-164px)/2)] overflow-hidden">
         <FeaturedCarousel />
