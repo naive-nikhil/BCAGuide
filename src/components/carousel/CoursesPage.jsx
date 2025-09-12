052d2f6bb9c4831b80bd70f92f48d6c564c3a7a0
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-12 10:58:09 +0530

    some changes

--- src/components/carousel/CoursesPage.jsx
+++ src/components/carousel/CoursesPage.jsx
@@ -1,11 +1,61 @@
 import { Link } from "react-router-dom";
 import semesters from "../../data/data.json";
 import { useAppContext } from "../../context/AppContext";
+import { useState } from "react";
+import downIcon from "../../assets/down.png";
 
-const CoursesPage = ({ sectionDesc }) => {
+const CoursesPage = ({ sectionDesc, baseUrl }) => {
   const { selectedSemester } = useAppContext();
+  const [selectedSession, setSelectedSession] = useState(
+    "July 2024 & January 2025"
+  );
+
+  const [openSessionSelect, setOpenSessionSelect] = useState(false);
   return (
-    <div className="w-full [&_h2]:cursor-pointer flex flex-col gap-4">
+    <div className="w-full [&_h2]:cursor-pointer flex flex-col gap-4 relative">
+      {baseUrl && baseUrl.includes("assignments") && (
+        <div className="text-gray-700 cursor-pointer select-none relative lg:absolute top-0 right-0">
+          <h2
+            onClick={() => setOpenSessionSelect(!openSessionSelect)}
+            className={`flex items-center justify-between gap-4 bg-white px-2 rounded-md border border-gray-300 ${
+              openSessionSelect && "rounded-b-none"
+            }`}
+          >
+            {selectedSession}
+            <img
+              src={downIcon}
+              className={`brightness-10 mb-[2px] transition-all duration-300 ease ${
+                openSessionSelect ? "rotate-180" : ""
+              }`}
+              width={12}
+            />
+          </h2>
+          {openSessionSelect && (
+            <ul className="absolute rounded-b-md top-full w-full right-0 bg-white z-10 shadow-xl border border-gray-300 flex flex-col">
+              <Link
+                to={"/assignments/2024-25"}
+                onClick={() => {
+                  setOpenSessionSelect(false);
+                  setSelectedSession("July 2024 & January 2025");
+                }}
+                className="p-2 text-nowrap hover:bg-green-200"
+              >
+                July 2024 & January 2025
+              </Link>
+              <Link
+                to={"/assignments/2023-24"}
+                onClick={() => {
+                  setOpenSessionSelect(false);
+                  setSelectedSession("July 2023 & January 2024");
+                }}
+                className="p-2 text-nowrap hover:bg-green-200"
+              >
+                July 2023 & January 2024
+              </Link>
+            </ul>
+          )}
+        </div>
+      )}
       <section>
         <h1 className="text-lg">Courses in {selectedSemester}</h1>
         <p className="text-gray-400">{sectionDesc}</p>
--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,5 +1,5 @@
 import backIcon from "../assets/back.png";
-import bcs012June2024 from "../assets/BCS012_JUNE2024.jpg";
+
 import { useState, useEffect } from "react";
 import HeroCarousel from "../components/HeroCarousel";
 import FeaturedCarousel from "../components/FeaturedCarousel";
@@ -8,11 +8,15 @@ import { Link } from "react-router-dom";
 
 import semesters from "../data/data.json";
 import { useParams } from "react-router-dom";
+import SemesterList from "../components/carousel/SemesterList";
+import Carousel from "../components/Carousel";
+import CoursesPage from "../components/carousel/CoursesPage";
+import DownloadAssignment from "../components/assignments/DownloadAssignment";
 
 const Assignments = () => {
   const [selectedSession, setSelectedSession] = useState("june");
   const [selectedYear, setSelectedYear] = useState("");
-  const { setPage, selectedSemester, setSelectedCourse, selectedCourse } =
+  const { setPage, selectedSemester, setSelectedCourse, selectedCourse, page } =
     useAppContext();
 
   const { session } = useParams();
@@ -23,80 +27,29 @@ const Assignments = () => {
   useEffect(() => {
     if (!courseCode && session) {
       setSelectedSession(session);
-      setPage(1);
+      setPage(
+        <CoursesPage sectionDesc="Assignments" baseUrl={"/assignments"} s />
+      );
     } else if (courseCode && session) {
       setSelectedCourse(courseCode.toUpperCase());
-      setPage(2);
+      setPage(<DownloadAssignment />);
     }
   }, [courseCode, session]);
-
-  const selectedCourseTitle = semesters
-    .find((sem) => sem.title === selectedSemester)
-    .subjects.find((sub) => sub.code === selectedCourse)?.title;
-
-  const selectedSessionCourseAssignmentLink = semesters
-    .find((sem) => sem.title === selectedSemester)
-    .subjects.find((sub) => sub.code === selectedCourse)?.assignments?.[
-    selectedSession
-  ];
-
-  console.log(selectedSessionCourseAssignmentLink);
   return (
-    <div className="flex flex-col">
-      <HeroCarousel
-        sectionHeading={"Assignments With Solution"}
-        sectionDesc={"assignments"}
-        totalPages={2}
-        baseUrl={"/assignments/2024-25"}
-      >
-        {/* Page - 2 */}
-        <div className="flex justify-between gap-4">
-          <div className="w-full flex flex-col justify-between">
-            <Link
-              to={`/assignments/${session}`}
-              className="p-2 w-fit cursor-pointer border rounded-full border-b-3 border-r-2 bg-violet-50 border-violet-500 hover:-translate-y-1 transition-all duration-300 ease"
-            >
-              <img src={backIcon} width={20} className="brightness-20" />
-            </Link>
-            <div>
-              <h1 className="text-lg flex items-center gap-2">
-                {selectedCourse}
-                <p className="bg-green-100 text-green-600 text-sm w-fit px-2 rounded">
-                  Solved
-                </p>
-              </h1>
-              <h2>{selectedCourseTitle}</h2>
-              <h3>Assignments - {session}</h3>
-
-              <div className="flex w-full gap-1 mt-4">
-                <a
-                  className="w-full text-center rounded py-2 cursor-pointer bg-blue-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-blue-600"
-                  target="_blank"
-                  href={selectedSessionCourseAssignmentLink}
-                >
-                  {" "}
-                  Download Assignment
-                </a>
-
-                <br />
-                <button
-                  className={`w-full rounded py-2 cursor-pointer bg-green-200 mt-2 hover:-translate-y-1 transition duration-300 ease-in-out text-green-600`}
-                >
-                  Download Solution
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="relative h-82 border rounded-md border-gray-300 bg-blue-200 w-full overflow-hidden group">
-            <img
-              src={bcs012June2024}
-              className="absolute object-cover -rotate-25 shadow-2xl right-0 -bottom-4 translate-y-1/2 translate-x-1/3 group-hover:scale-105 transition-transform duration-300 ease-in-out"
-            />
-          </div>
-        </div>
-      </HeroCarousel>
-      <FeaturedCarousel />
-    </div>
+    <>
+      <section className="h-full lg:h-[calc(calc(100vh-164px)/2)] overflow-hidden flex flex-col gap-2">
+        <h1 className="text-xl text-gray-700">
+          Previous Year Question Papers With Solutions
+        </h1>
+        <Carousel
+          sidebarComponent={<SemesterList baseUrl={"/assignments"} />}
+          pages={page}
+        />
+      </section>
+      <section className="h-[calc(calc(100vh-164px)/2)] overflow-hidden">
+        <FeaturedCarousel />
+      </section>
+    </>
   );
 };
 
