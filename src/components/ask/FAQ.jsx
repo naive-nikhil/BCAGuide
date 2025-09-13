5c9ee79bf124aa2316ef567b8a6ad52375ec4c0e
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-13 19:23:47 +0530

    some changes

--- src/components/ask/FAQ.jsx
+++ src/components/ask/FAQ.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import { useState } from "react";
 
 const faqs = [
+  { que: "What is your Name!", ans: "The name's Bond! James Bond." },
+  { que: "What is your Name!", ans: "The name's Bond! James Bond." },
+  { que: "What is your Name!", ans: "The name's Bond! James Bond." },
   { que: "What is your Name!", ans: "The name's Bond! James Bond." },
   { que: "What is your Name!", ans: "The name's Bond! James Bond." },
   { que: "What is your Name!", ans: "The name's Bond! James Bond." },
 ];
 
 const FAQ = () => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleFAQ = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-xl">Frequently Asked Questions</h2>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+      <ul className="grid grid-cols-1 gap-4">
         {faqs &&
           faqs.map((faq, index) => (
-            <Link
-              to={`${material.id.toLowerCase().replace(/\s/g, "")}`}
+            <li
+              onClick={() => toggleFAQ(index)}
               key={index}
-              className="border p-2 border-gray-200 hover:-translate-y-1 transition-all duration-300 ease rounded border-b-3 text-blue-600 cursor-pointer"
+              className="border p-2 border-gray-200 transition-all duration-300 ease rounded border-b-3 cursor-pointer"
             >
-              {faq.que} - {faq.ans}
-            </Link>
+              <h2>{faq.que}</h2>
+              {openIndex === index && (
+                <p className="text-gray-400">{faq.ans}</p>
+              )}
+            </li>
           ))}
       </ul>
     </div>
