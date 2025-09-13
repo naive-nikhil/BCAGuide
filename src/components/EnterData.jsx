bbc507426ce94638ea1f687e90816b62aa170f74
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-14 01:07:10 +0530

    some changes

--- src/components/EnterData.jsx
+++ src/components/EnterData.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { useAppContext } from "../context/AppContext";
 
 const EnterData = () => {
-  const { showForm } = useAppContext();
+  const { showForm, setShowForm } = useAppContext();
   return (
     showForm && (
       <div className="absolute top-0 left-0 h-dvh w-full bg-black/70 flex justify-center items-center z-999">
-        <div className="h-100 w-80 bg-white rounded-md">Enter Data Form</div>
+        <div className="h-100 w-80 bg-white rounded-md relative">
+          <span
+            onClick={() => setShowForm(false)}
+            className="absolute top-2 right-2 cursor-pointer"
+          >
+            Close
+          </span>
+        </div>
       </div>
     )
   );
