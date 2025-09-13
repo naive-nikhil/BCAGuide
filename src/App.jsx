407c70cc630570f36be10219f968af64b85fa1ca
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-14 00:51:06 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,8 @@ import Project from "./pages/Project";
 import About from "./pages/About";
 import AskDoubt from "./pages/AskDoubt";
 import Notifications from "./pages/Notifications";
-import { useEffect } from "react";
 
 const App = () => {
-  let startY = 0;
-  useEffect(() => {
-    const handleTouchStart = (e) => {
-      if (e.touches.length === 3) {
-        startY = e.touches[0].clientY;
-      }
-    };
-
-    const handleTouchEnd = (e) => {
-      if (e.changedTouches.length === 3) {
-        let endY = e.changedTouches[0].clientY;
-        if (endY - startY > 80) {
-          alert("Hello");
-        }
-      }
-    };
-
-    document.addEventListener("touchstart", handleTouchStart);
-    document.addEventListener("touchend", handleTouchEnd);
-
-    return () => {
-      document.removeEventListener("touchstart", handleTouchStart);
-      document.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, []);
   return (
     <>
       <Routes>
