172e74f00f331c4fb687ba9dd432a425ac27c6c0
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-13 16:21:28 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,11 @@ import FeaturedCarousel from "../components/FeaturedCarousel";
 const Home = () => {
   return (
     <>
-      <section className="h-[calc(calc(100vh-164px)/2)] rounded-md overflow-hidden">
-        <img
-          src={heroImg}
-          className="h-full w-full object-cover object-bottom"
-          alt="Image representing a wooden block on table with text 'Do What You Love' written on it."
-        />
-      </section>
-      <section className="h-[calc(calc(100vh-164px)/2)] overflow-hidden">
-        <FeaturedCarousel />
-      </section>
+      <img
+        src={heroImg}
+        className="h-full w-full object-cover object-bottom"
+        alt="Image representing a wooden block on table with text 'Do What You Love' written on it."
+      />
     </>
   );
 };
