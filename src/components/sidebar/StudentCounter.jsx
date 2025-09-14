47a1efbd7b53844195914ca636bc85b01d11ed51
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-15 00:45:00 +0530

    some changes

--- src/components/sidebar/StudentCounter.jsx
+++ src/components/sidebar/StudentCounter.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import studentLogo from "../../assets/student.png";
+import { db } from "../../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
-const StudentCounter = ({ count }) => {
+const StudentCounter = () => {
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    const ref = doc(db, "stats", "visits");
+    const updateVisits = async () => {
+      const snap = getDoc(ref);
+      const currentCount = snap.data().count;
+
+      await updateDoc(ref, { count: currentCount + 1 });
+
+      setCount(currentCount + 1);
+    };
+
+    if (!sessionStorage.getItem("visited")) {
+      updateVisits();
+      sessionStorage.setItem("visited", "true");
+    }
+  }, []);
   return (
     <section className="flex justify-center items-center gap-2 bg-white rounded h-[40px] border border-gray-300">
       <img
