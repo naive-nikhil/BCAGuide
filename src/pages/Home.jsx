27afbe0fc8c9ea3f6e6de4a91f6408f6f92df200
naive-nikhil <68939870+naive-nikhil@users.noreply.github.com>
2025-09-26 02:29:50 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <title>BCAGuide - IGNOU Papers, Assignments & Resources</title>
       </Helmet>
       <img
+        fetchPriority="high"
         src="/hero.webp"
         className="h-full w-full object-cover object-bottom bg-violet-100"
         alt="Image representing a wooden block on table with text 'Do What You Love' written on it"
